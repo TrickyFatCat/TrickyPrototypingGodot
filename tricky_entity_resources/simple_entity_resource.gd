4b6796f5b26e69439b60bd37645b6b77024362aa
TrickyFatCat <tricky.fat.cat@gmail.com>
2023-03-29 00:26:32 +0300

    Fix getters and setters

--- tricky_entity_resources/simple_entity_resource.gd
+++ tricky_entity_resources/simple_entity_resource.gd
@@ -18,23 +18,9 @@ signal max_value_decreased(new_max_value: int, delta_value: int)
 	get:
 		return initial_value
 		
-@export var min_value : int = 0 :
-	set(new_value):
-		pass
-	get:
-		return min_value
-		
-@export var max_value : int = 100 :
-	set(new_value):
-		pass
-	get:
-		return max_value
-
-@onready var value : int = max(initial_value, 0) : 
-	set(new_value):
-		pass
-	get:
-		return value
+@export var min_value : int = 0
+@export var max_value : int = 100
+@onready var _value : int = max(initial_value, 0)
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_DISABLED
@@ -42,31 +28,31 @@ func _ready() -> void:
 
 
 func increase_value(amount: int, clamp_to_max: bool = true) -> bool:
-	if amount <= 0 || clamp_to_max && value >= max_value:
+	if amount <= 0 || clamp_to_max && _value >= max_value:
 		return false
 
 	if clamp_to_max:
-		var delta_value : int = max_value - value
+		var delta_value : int = max_value - _value
 		amount = delta_value if amount > delta_value else amount
 
-	value += amount
-	value_increased.emit(value, amount)
+	_value += amount
+	value_increased.emit(_value, amount)
 
 	return true
 
 
 func decrease_value(amount: int) -> bool:
-	if amount <= 0 || value <= min_value:
+	if amount <= 0 || _value <= min_value:
 		return false
 
 	if min_value <= 0:
-		var delta_value : int = abs(min_value - value)
+		var delta_value : int = abs(min_value - _value)
 		amount = delta_value if amount > delta_value else amount
 
-	value -= amount
-	value_decreased.emit(value, amount)
+	_value -= amount
+	value_decreased.emit(_value, amount)
 	
-	if value <= min_value:
+	if _value <= min_value:
 		value_reached_minimum.emit()
 
 	return true
@@ -81,8 +67,8 @@ func increase_min_value(amount: int, clamp_value : bool = true) -> bool:
 	min_value += amount
 	min_value_increased.emit(min_value, amount)
 
-	if value < min_value && clamp_value:
-		delta_value = abs(min_value - value)
+	if _value < min_value && clamp_value:
+		delta_value = abs(min_value - _value)
 		increase_value(delta_value)
 
 	return true
@@ -106,7 +92,7 @@ func increase_max_value(amount: int, clamp_value: bool = false) -> bool:
 	max_value_increased.emit(max_value, amount)
 
 	if clamp_value:
-		var delta_value : int = max_value - value
+		var delta_value : int = max_value - _value
 		increase_value(delta_value)
 
 	return true
@@ -126,7 +112,7 @@ func decrease_max_value(amount: int, clamp_value: bool = true) -> bool:
 	max_value_decreased.emit(max_value, amount)
 
 	if clamp_value:
-		delta_value = abs(max_value - value)
+		delta_value = abs(max_value - _value)
 		decrease_value(delta_value)
 
 	return true
@@ -136,4 +122,4 @@ func get_relative_value() -> float:
 	if max_value == 0:
 		return 0
 
-	return float(value) / float(max_value - min_value)
+	return float(_value) / float(max_value - min_value)
