a9a65c89bb4a99cf14bf981fa1d9881802331f7a
TrickyFatCat <tricky.fat.cat@gmail.com>
2023-03-29 23:30:51 +0300

    Rename script file

--- tricky_keyring/LockKey.gd
+++
@@ -1,7 +0,0 @@
-class_name LockKey
-extends Resource
-
-@export var name : String = "LockKey Name"
-@export_color_no_alpha var color : Color = Color.RED
-@export var destroy_on_use : bool = false
-@export var is_unique : bool = true
