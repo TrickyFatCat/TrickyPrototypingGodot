245e1dfd97fe5a560c2f63403f8c7c45ca5a2e42
TrickyFatCat <tricky.fat.cat@gmail.com>
2023-03-30 23:05:03 +0300

    Add comments

--- tricky_keyring/lock_key.gd
+++ tricky_keyring/lock_key.gd
@@ -1,7 +1,13 @@
 class_name LockKey
 extends Resource
 
+## A class which contains lock key data
+
 @export var name : String = "LockKey Name"
 @export_color_no_alpha var color : Color = Color.RED
+
+## If true, the key will be destroyed on use_lock_key call in Keyring
 @export var destroy_on_use : bool = false
+
+## If true, only one key can be stored in Keyring
 @export var is_unique : bool = true
