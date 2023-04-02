4d1f46d11ba308ef7cad365968e4468a1eacb4fd
TrickyFatCat <tricky.fat.cat@gmail.com>
2023-04-02 19:25:14 +0300

    Add proper _init iplementation

--- tricky_interaction/interaction_data.gd
+++ tricky_interaction/interaction_data.gd
@@ -17,3 +17,12 @@ enum InteractionType{
 
 ## Used for sorting in the interaction queue
 @export var interaction_weight : int = 1
+
+
+func _init(hint: String, type : InteractionType, duration : float, weight: int) -> void:
+	resource_local_to_scene = true 
+	interaction_hint = hint
+	interaction_type = type
+	interaction_duration = duration
+	interaction_duration = max(interaction_duration, 0.0)
+	interaction_weight = weight
